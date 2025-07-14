09725b97e7286ae6e1e5263098b1ebb68b28bfc6
sAmH365 <npower5377@naver.com>
2025-07-15 08:49:25 +0900

    bak2747

---
+++ src/bak2747/bak2747.java
@@ -0,0 +1,32 @@
+package bak2747;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bak2747 {
+
+  static int[] memo;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+
+    memo = new int[n + 1];
+    memo[1] = 1;
+
+    System.out.println(fibo(n));
+  }
+
+  static int fibo(int n) {
+    if (n == 0 || n == 1) return memo[n];
+
+    if (memo[n] != 0) return memo[n];
+
+    memo[n] = fibo(n-1) + fibo(n-2);
+
+    return memo[n];
+  }
+}
